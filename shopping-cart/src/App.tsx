b31a6085e13b9e5bf48f3c2e2f86dacf6604def5
Luis Andia <luisandiamamani@gmail.com>
2020-08-17 16:45:47 -0500

    adding header components and link to home and shome

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
